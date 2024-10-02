4fd6a6a0e6bd12c18a96e9f814518baea9a969b1
PANCHO7532 <pancho7532@p7com.net>
2024-10-02 04:26:53 -0300

    stuff5

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const fastify = require("fastify")();
+const awsLambda = require("@fastify/aws-lambda");
 fastify.register(require("./routes"));
 /*if(require.main != "module") {
     
@@ -14,6 +15,7 @@ if(process.env.NETLIFY || process.env.NETLIFY_LOCAL) {
 } else {
     fastify.listen({port: 3000, host: "0.0.0.0"}, async() => { console.log("ready"); });
 }
-exports.handler = require("serverless-http")(fastify);
+//exports.handler = require("serverless-http")(fastify);
+exports.handler = awsLambda(fastify);
 //console.log(require);
 //console.log(require.main);
