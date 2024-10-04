de8d8806090922cece208a1a6162b83823e08387
PANCHO7532 <pancho7532@p7com.net>
2024-10-04 04:01:26 -0300

    what3

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ fastify.register(require("./routes/asd"));
 //
 //
 //console.log(process.env);
-if(process.env.SERVERLESS == "1" || process.env.NETLIFY_LOCAL) {
+if(true) {
     exports.handler = awsLambda(fastify, {binaryMimeTypes: [
         "application/octet-stream",
         "image/png",
